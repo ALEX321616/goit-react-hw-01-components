3d1afc1ba189ff94429d7797876e5d9a033b0cb6
ALEX321616 <zaplev@ukr.net>
2022-02-09 23:27:32 +0200

    +

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -36,12 +36,4 @@ Statistics.propTypes = {
   ),
 };
 
-// Statistics.propTypes = {
-//   PropTypes.shape({
-//     id: PropTypes.string.isRequired,
-//     label: PropTypes.string.isRequired,
-//     percentage: PropTypes.number.isRequired,
-//   }),
-// };
-
 export default Statistics;
